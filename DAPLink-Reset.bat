e98a5465138d558d452b247e949aec27833b211f
pvvx <pvvx@mail.ru>
2017-01-31 15:50:34 +0300

    fix err

--- DAPLink-Reset.bat
+++ DAPLink-Reset.bat
@@ -1,4 +1,4 @@
 @cd flasher
-openocd -f interface/cmsis-dap.cfg -c "adapter_khz 1000" -f rtl8710.ocd -f cortex.ocd -c "init" -c "reset halt" -c "cortex_reboot" -c "shutdown"
+openocd -f interface/cmsis-dap.cfg -c "adapter_khz 1000" -f rtl8710.ocd -f cortex.ocd -c "init" -c "reset halt" -c "restart_from_falsh" -c "shutdown"
 rem 
 
